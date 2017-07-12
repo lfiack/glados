ceb000901209152148fa036e613b1ffef56ab7ba
Laurent Fiack <laurent.fiack@gmail.com>
2017-07-12 21:54:17 +0200

    screenshot

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 
 ## Requirements
+sudo apt-get install python-pip
+sudo apt-get install python-tk
 sudo apt-get install python-imaging-tk
+
+pip install -r requirements.txt
--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ import Image, ImageTk
 import pymouse as pm
 import pyscreenshot as ImageGrab
 
+from ast import literal_eval
 
 class MainFrame(tk.Tk):
     def __init__(self):
@@ -22,9 +23,11 @@ class MainFrame(tk.Tk):
 
         self.dscanStartPositionVar = tk.StringVar()
         self.dscanStartPositionEntry = tk.Entry(self.dscanFrame, textvariable=self.dscanStartPositionVar, width=9)
+        self.dscanStartPositionEntry.insert(0,"0,0")
         self.dscanStartPositionEntry.pack(in_=self.dscanFrame, side=tk.LEFT)
         self.dscanStopPositionVar = tk.StringVar()
         self.dscanStopPositionEntry = tk.Entry(self.dscanFrame, textvariable=self.dscanStopPositionVar, width=9)
+        self.dscanStopPositionEntry.insert(0,"500,500")
         self.dscanStopPositionEntry.pack(in_=self.dscanFrame, side=tk.LEFT)
 
         self.dscanDisplayButton = tk.Button(self.dscanFrame, text="Display", command=self.imageFrame)
@@ -36,11 +39,8 @@ class MainFrame(tk.Tk):
         self.mousePositionLabel = tk.Label(self,textvariable=self.mousePositionVar)
         self.mousePositionLabel.pack()
 
-#        self.canvas = tk.Canvas(self)
-#        self.canvas.pack()
-#        self.im=ImageGrab.grab(bbox=(10,10,500,500))
-#        self.image = ImageTk.PhotoImage(self.im)
-#        self.imagesprite = self.canvas.create_image(500,500,image=self.image)
+        self.winOpen = 0
+        self.screenshot()
 
         self.update()
 
@@ -51,20 +51,36 @@ class MainFrame(tk.Tk):
 
     def imageFrame(self):
         # create child window
-        self.win = tk.Toplevel()
-        self.win.title("Toto")
-        self.canvas = tk.Canvas(self.win, width=500, height=500)
+        self.win = SubFrame(self)
+        self.winOpen = 1
+
+    def screenshot(self):
+        print("Taking screenshot")
+        posStart=literal_eval(self.dscanStartPositionEntry.get())
+        posStop=literal_eval(self.dscanStopPositionEntry.get())
+        print(str(posStart))
+        print(str(posStop))
+        self.pilImage=ImageGrab.grab(bbox=(posStart[0],posStart[1],posStop[0],posStop[0]))
+        if (self.winOpen):
+            self.win.update(self.pilImage)
+        self.after(1000, self.screenshot)
+
+class SubFrame(tk.Toplevel):
+    def __init__(self,root):
+        tk.Toplevel.__init__(self)
+        self.root=root
+        self.title("SubFrame")
+        self.canvas = tk.Canvas(self, width=500, height=500)
         self.canvas.grid(row=0, column=0)
+        self.protocol("WM_DELETE_WINDOW", self.onClosing)
 
-        self.pilImage = ImageGrab.grab(bbox=(0,0,500,500))
-        self.image = ImageTk.PhotoImage(self.pilImage)
+    def update(self,pilImage):
+        print("Updating SubFrame")
+        self.image = ImageTk.PhotoImage(pilImage)
         self.sprite = self.canvas.create_image(0,0,image=self.image,anchor=tk.NW)
-
-        self.updateImageFrame()
-
-    def updateImageFrame(self):
-        print("a")
-        self.pilImage=ImageGrab.grab(bbox=(0,0,500,500))
-        self.image = ImageTk.PhotoImage(self.pilImage)
         self.canvas.itemconfig(self.sprite, image = self.image)
-        self.after(1000, self.updateImageFrame)
+
+    def onClosing(self):
+        print("Closing SubFrame")
+        self.root.winOpen=0
+        self.destroy()
